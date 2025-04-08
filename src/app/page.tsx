6e2cee0b8a502cc0ab922910662e04b36f79236b
pLogicador <pedroemanuelsm0@gmail.com>
2025-04-07 21:50:08 -0300

    feat: implement game of the day section with dynamic fetch and link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,52 @@
+import { Container } from "@/components/container";
+import { GameProps } from "@/utils/types/game";
 import Image from "next/image";
+import Link from "next/link";
+import { BsArrowRightSquare } from "react-icons/bs";
 
-export default function Home() {
+async function getPlgDeck() {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game_day`,
+      { next: { revalidate: 350 } }
+    );
+    return response.json();
+  } catch (err) {
+    throw new Error("Failed to fetch data");
+  }
+}
+
+export default async function Home() {
+  const plgdeck: GameProps = await getPlgDeck();
+
   return (
-    <div className="">
-      <main className="flex">
-        <h1>Hello Test</h1>
-      </main>
-    </div>
+    <main className="w-full">
+      <Container>
+        <h1 className="text-center font-bold text-xl mt-8 mb-5">
+          What are you in the mood to play?
+        </h1>
+
+        <Link href={`/game/${plgdeck.id}`}>
+          <section className="w-full bg-black rounded-lg">
+            <div className="w-full max-h-96 h-96 relative rounded-lg">
+              <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+                <p className="font-bold text-xl text-white">{plgdeck.title}</p>
+                <BsArrowRightSquare size={24} color="#fff" />
+              </div>
+
+              <Image
+                src={plgdeck.image_url}
+                alt={plgdeck.description}
+                priority={true}
+                quality={100}
+                fill={true}
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 44vw"
+                className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+              />
+            </div>
+          </section>
+        </Link>
+      </Container>
+    </main>
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FaGamepad } from "react-icons/fa";
+import { LiaGamepadSolid } from "react-icons/lia";
+
 export function Header() {
   return (
-    <header>
-      <h1>Header test</h1>
+    <header className="w-full h-28 bg-gray-800 text-amber-50 px-2">
+      <div className="max-w-screen-xl mx-auto flex justify-center items-center h-28 sm:justify-between">
+        <nav className="flex justify-center items-center gap-4">
+          <Link href="/">
+            <Image
+              src="/logo-plg.eckpng-removebg-preview.png"
+              alt="PlgDeck website logo"
+              quality={100}
+              width={140}
+              height={40}
+              priority
+              className="w-full"
+            />
+          </Link>
+
+          <Link href="/">Games</Link>
+          <Link href="/profile">Profile</Link>
+        </nav>
+
+        <div className="hidden sm:flex justify-center items-center">
+          <Link href="/profile">
+            <FaGamepad size={34} color="#000" />
+          </Link>
+        </div>
+      </div>
     </header>
   );
 }
